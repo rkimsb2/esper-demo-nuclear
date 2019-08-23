21cc809af1b5d9bba5f5d8d6f1c6df397149fb33
rokwoon.kim <k862390@gmail.com>
2019-08-23 17:07:20 +0900

    modified as spring base

--- src/main/java/com/cor/cep/BootConfig.java
+++ src/main/java/com/cor/cep/BootConfig.java
@@ -1,16 +1,31 @@
 package com.cor.cep;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextStartedEvent;
 
 import com.cor.cep.util.RandomTemperatureEventGenerator;
 
 @Configuration
-public class BootConfig {
+public class BootConfig implements ApplicationListener<ContextStartedEvent> {
+	
+	@Value("${event.generate.number:1000}")
+	private long noOfTemperatureEvents;
+	
+	@Autowired
+	private RandomTemperatureEventGenerator eventGenerator;
 	
 	@Bean
 	public RandomTemperatureEventGenerator eventGenerator() {
 		return new RandomTemperatureEventGenerator();
+	}
+
+	@Override
+	public void onApplicationEvent(ContextStartedEvent event) {
+		eventGenerator.startSendingTemperatureReadings(noOfTemperatureEvents);
 	}
 
 }
--- src/main/java/com/cor/cep/StartDemo.java
+++ src/main/java/com/cor/cep/StartDemo.java
@@ -1,45 +1,13 @@
 package com.cor.cep;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
-import com.cor.cep.util.RandomTemperatureEventGenerator;
-
-/**
- * Entry point for the Demo. Run this from your IDE, or from the command line using 'mvn exec:java'.
- */
 @SpringBootApplication
 public class StartDemo {
 
-    /** Logger */
-    private static Logger LOG = LoggerFactory.getLogger(StartDemo.class);
-
-    
-    /**
-     * Main method - start the Demo!
-     */
     public static void main(String[] args) throws Exception {
-
-        LOG.debug("Starting...");
-
-        long noOfTemperatureEvents = 1000;
-
-        if (args.length != 1) {
-            LOG.debug("No override of number of events detected - defaulting to " + noOfTemperatureEvents + " events.");
-        } else {
-            noOfTemperatureEvents = Long.valueOf(args[0]);
-        }
-
-        // Load spring config
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(StartDemo.class, args);
-        
-        // Start Demo
-        RandomTemperatureEventGenerator generator = (RandomTemperatureEventGenerator) applicationContext.getBean("eventGenerator");
-        generator.startSendingTemperatureReadings(noOfTemperatureEvents);
-
+        SpringApplication.run(StartDemo.class, args).start();
     }
 
 }
